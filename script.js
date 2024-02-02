f9b374480576e03836ec5815005e0b05672d3a2e
NikhilPeddireddy <139708292+NikhilPeddireddy@users.noreply.github.com>
2024-02-03 00:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const newTodoInput = document.getElementById('newTodoInput');
+  const addTodoBtn = document.getElementById('addTodoBtn');
+  const todoList = document.getElementById('todoList');
+
+  addTodoBtn.addEventListener('click', function () {
+    const newTodoText = newTodoInput.value.trim();
+
+    if (newTodoText !== '') {
+      const newTodoItem = document.createElement('li');
+      newTodoItem.textContent = newTodoText;
+
+      todoList.appendChild(newTodoItem);
+      newTodoInput.value = '';
+    }
+  });
+});
+
 
